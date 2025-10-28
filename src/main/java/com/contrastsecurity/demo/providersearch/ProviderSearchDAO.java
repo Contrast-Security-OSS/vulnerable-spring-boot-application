dafe4176e591eceff4cec7220bb8c47b59c03b9f
Syahrul Fathi <syahrul.fathi@doku.com>
2025-10-28 08:53:34 +0700

    fixing sql injection

--- src/main/java/com/contrastsecurity/demo/providersearch/ProviderSearchDAO.java
+++ src/main/java/com/contrastsecurity/demo/providersearch/ProviderSearchDAO.java
@@ -14,8 +14,9 @@ public class ProviderSearchDAO {
 
     public List<Object[]> getProvidersInZipCode(String zipCode) {
         EntityManager em = entityManagerFactory.createEntityManager();
-        String q = "select * from PROVIDERS where public_listing is true and zip_code = '" + zipCode + "'";
+        String q = "select * from PROVIDERS where public_listing = true and zip_code = ?1";
         Query query = em.createNativeQuery(q);
+        query.setParameter(1, zipCode);
         List<Object[]> results = query.getResultList();
         return results;
     }
